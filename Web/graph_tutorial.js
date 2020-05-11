12117e971d908d43419191f96e527acf12c4f5e6
Paulin Kantue <pkantue@gmail.com>
2020-05-11 18:13:29 +0100

    dev script to migrate to responsive iframes using JS and CSS.

---
+++ Web/graph_tutorial.js
@@ -0,0 +1,45 @@
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+            ['Mushrooms', 3],
+            ['Onions', 1],
+            ['Olives', 1],
+            ['Zucchini', 1],
+            ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {
+            title: "Density of Precious Metals, in g/cm^3",
+            // height: 400
+	    //width: 1100, <--- DON'T SET THIS OPTION, it will be set automatically depending on the size of the container div
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+
+$(window).resize(function(){
+  drawChart();
+});
+</script>
+
+<!--Div that will hold the pie chart-->
+<div id="chart_div" class="mt-3" style="height: 100%; width: 100%;"></div>
