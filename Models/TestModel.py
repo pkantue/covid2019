fa16a2e1d695d7d741d78cbc41c5dbf31c72f9da
Paulin Kantue <pkantue@gmail.com>
2020-06-05 21:59:58 +0100

    script updated to load googlesheet directly.

--- Models/TestModel.py
+++ Models/TestModel.py
@@ -1,5 +1,13 @@
 #this is a script to model SEIR model the SEIR dynamics and predict apex of active cases. 
 
-import numpy as np 
+import pandas 
+googleSheetId = '1drFy1W0FCryC_yPW_hUDNKBmQAkGJ-JMzKMEi_7E1nE'
+worksheetName = 'Confirmed'
+URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(
+	googleSheetId,
+	worksheetName
+)
 
-R = 5
+Confirmed = pandas.read_csv(URL)
+#debug to printout
+print(Confirmed) 
